029bd0646281a315116312ef960dd9a13f3b1587
Andrei <lay4ik@gmail.com>
2023-09-22 16:00:51 +0300

    Add Rotator

---
+++ Task 1/Assets/Scripts/Rotator.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public bool x;
+    public bool y;
+    public bool z;
+
+    public float velocity = 90;
+
+    private Vector3 axis2;
+
+    void Update()
+    {
+        axis2 = new Vector3(Convert.ToInt32(x), Convert.ToInt32(y), Convert.ToInt32(z));
+        transform.rotation *= Quaternion.AngleAxis(velocity * Time.deltaTime, axis2);
+
+    }
+
+}
