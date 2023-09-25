9e147c217efa654db060f494aa2c0d609a66fd47
Andrei <lay4ik@gmail.com>
2023-09-25 17:27:38 +0300

    Added button open/close window

---
+++ Task 1/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof (Button))]
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject CloseWindow;
+    public GameObject OpenWindow;
+
+    private Button btn;
+
+    private void Start()
+    {
+        if (OpenWindow == null)
+        {
+            Debug.Log("Missing ref openedWindow on " + gameObject.name);
+            return;
+        }
+
+        if (CloseWindow == null)
+        {
+            Debug.Log("Missing ref closedWindow on " + gameObject.name);
+            return;
+        }
+
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(() => OpenClose());
+    }
+
+    private void OpenClose()
+    {
+        OpenWindow.SetActive(true);
+        CloseWindow.SetActive(false);
+    }
+}
