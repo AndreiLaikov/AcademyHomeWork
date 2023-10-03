acbcf4f868f982fe2e159a97ee57782300d8f1cc
Andrei <lay4ik@gmail.com>
2023-10-03 12:35:37 +0300

    Create Shoot with simple bullet and switch bulletsType

---
+++ Task 1/Assets/Scripts/BulletType.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BulletType : MonoBehaviour
+{
+    public enum bTypes
+    {
+        None,
+        Simple,
+        Granade,
+        Tennis
+    }
+
+    public bTypes bType;
+}
--- Task 1/Assets/Scripts/RobotShooter.cs
+++ Task 1/Assets/Scripts/RobotShooter.cs
@@ -4,10 +4,48 @@ public class RobotShooter : MonoBehaviour
 {
     public GameObject simpleBulletPrefab;
 
+    public BulletType currentBullet;
+
+    private void Start()
+    {
+        currentBullet = GetComponent<BulletType>();
+    }
+
     [ContextMenu("SimpleShoot")]
     public void SimpleBulletShoot()
     {
         var pos = transform.position + Vector3.forward;
         Instantiate(simpleBulletPrefab, pos, Quaternion.identity);
     }
+
+    public void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+            Shoot();
+    }
+
+    private void Shoot()
+    {
+        switch (currentBullet.bType)
+        {
+            case BulletType.bTypes.Simple:
+                SimpleBulletShoot();
+                break;
+           
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {   
+        if (other.GetComponent<BulletType>())
+            currentBullet.bType = other.GetComponent<BulletType>().bType;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        currentBullet.bType = BulletType.bTypes.None;
+    }
+
+
+
 }
