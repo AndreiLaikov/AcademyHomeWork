4a5ef2619609976d770012b3de9f1f2ae4adc699
Andrei <lay4ik@gmail.com>
2023-10-09 10:23:05 +0300

    Controller from the lesson

--- Task 1/Assets/Scripts/CharController.cs
+++ Task 1/Assets/Scripts/CharController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class CharController : MonoBehaviour
 {
     public float MovementSpeed = 10f;
+    public float SprintSpeed = 20f;
     public float RotationSpeed = 0.1f;
 
     private CharacterController controller;
     private Camera charCamera;
     private float rotationAngle = 0f;
+
+    private bool isSprint;
 
     public CharacterController Controller
     {
@@ -24,23 +27,35 @@ public class CharController : MonoBehaviour
 
     private void Update()
     {
+
+    }
+
+    private void Move()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        isSprint = Input.GetKey(KeyCode.LeftShift);
 
         Vector3 movement = new Vector3(horizontal, 0, vertical);
         Vector3 rotatedMovement = Quaternion.Euler(0f, CharCamera.transform.rotation.eulerAngles.y, 0f) * movement.normalized;
 
-        Controller.Move(rotatedMovement * MovementSpeed * Time.deltaTime);
+        float currentSpeed = isSprint ? SprintSpeed : MovementSpeed;
 
-        if(rotatedMovement.sqrMagnitude > 0.0f)
+        Controller.Move(rotatedMovement * currentSpeed * Time.deltaTime);
+
+        if (rotatedMovement.sqrMagnitude > 0.0f)
         {
-            rotationAngle = Mathf.Atan2(rotatedMovement.x, rotatedMovement.y) * Mathf.Rad2Deg;
+            rotationAngle = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
         }
 
         Quaternion currentRot = Controller.transform.rotation;
         Quaternion targetRot = Quaternion.Euler(0, rotationAngle, 0);
 
         Controller.transform.rotation = Quaternion.Lerp(currentRot, targetRot, RotationSpeed);
+    }
+
+    private void Jump()
+    {
 
     }
 }
