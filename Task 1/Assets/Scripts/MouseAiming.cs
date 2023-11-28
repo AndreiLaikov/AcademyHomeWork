f6a30bc477e972cda4afd412637c10fb53eca46c
Андрей В. Лайков <lay4ik@gmail.com>
2023-11-28 14:08:56 +0300

    fixed moving and rotations

--- Task 1/Assets/Scripts/MouseAiming.cs
+++ Task 1/Assets/Scripts/MouseAiming.cs
@@ -2,34 +2,43 @@ using UnityEngine;
 
 public class MouseAiming : MonoBehaviour
 {
-    public float xSensitive;
-    public float ySensitive;
-    private float yRot;
-    private float xRot;
-    private Rigidbody rBody;
+    public float Sensitivity;
+    public float MaxAngle;
+
+    private Vector3 halfScreen;
+    private Vector3 deltaMousePos;
+    private Vector3 prevMousePos;
 
     private void Start()
     {
-        rBody = GetComponent<Rigidbody>();
-    }
+        Vector3 screen = new Vector3();
+        screen.x = Screen.width;
+        screen.y = Screen.height;
 
-    private void Aim()
-    {
-        xRot -= Input.GetAxis("Mouse Y") * xSensitive;
-        xRot = Mathf.Clamp(xRot, -40, 10);
-
-        yRot = rBody.rotation.eulerAngles.y;
-        yRot += Input.GetAxis("Mouse X") * ySensitive;
-
-        rBody.rotation = Quaternion.Euler(xRot, yRot, 0);
+        halfScreen = screen / 2;
     }
 
     private void Update()
     {
-        rBody.isKinematic = Input.GetMouseButton(0);
+        Aim();
+    }
 
-        if (Input.GetMouseButton(0))
-            Aim();
-       
+    private void Aim()
+    {
+        var mousePos = Input.mousePosition - halfScreen; //mousepos in +-half screen
+        Vector3 mousePosNormalize = new Vector3(mousePos.x / halfScreen.x, mousePos.y / halfScreen.y, 0);
+
+        deltaMousePos = mousePosNormalize - prevMousePos;
+        prevMousePos = mousePosNormalize;
+
+        var targetRotationX = Quaternion.AngleAxis(-deltaMousePos.y * Sensitivity, transform.right);
+
+        Quaternion resultQuat = targetRotationX * transform.rotation;
+
+        if (Quaternion.Angle(transform.rotation, resultQuat) < MaxAngle)
+        {
+            transform.rotation = resultQuat;
+        }
+
     }
 }
--- Task 1/Assets/Scripts/RobotMover.cs
+++ Task 1/Assets/Scripts/RobotMover.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class RobotMover : MonoBehaviour
 {
     public float moveVelocity = 5.0f;
-    public float rotateValocity = 1.0f;
-
+    public float rotateVelocity = 1.0f;
     private Rigidbody rBody;
-
+    private Vector3 halfScreen;
+    private Vector3 deltaMousePos;
+    private Vector3 prevMousePos;
 
     private void Start()
     {
         rBody = GetComponent<Rigidbody>();
+
+        Vector3 screen = new Vector3();
+        screen.x = Screen.width;
+        screen.y = Screen.height;
+
+        halfScreen = screen / 2;
     }
 
     private void Update()
@@ -19,22 +26,25 @@ public class RobotMover : MonoBehaviour
         Rotate();
     }
 
-    private void Rotate()
-    {
-        float sideforce = Input.GetAxis("Horizontal") * rotateValocity;
-        if (sideforce != 0)
-        {
-            rBody.angularVelocity = new Vector3(0, sideforce, 0);
-        }
-    }
-
     private void Move()
     {
-        float forwardforce = Input.GetAxis("Vertical") * moveVelocity;
-        if (forwardforce!=0)
-        {
-            rBody.velocity = rBody.transform.forward * forwardforce;
-        }
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = rBody.transform.forward * vertical + rBody.transform.right * horizontal;
+        rBody.velocity = movement * moveVelocity;
+    }
+
+    private void Rotate()
+    {
+        var mousePos = Input.mousePosition - halfScreen; //mousepos in +-half screen
+        Vector3 mousePosNormalize = new Vector3(mousePos.x / halfScreen.x, mousePos.y / halfScreen.y, 0);
+
+        deltaMousePos = mousePosNormalize - prevMousePos;
+        prevMousePos = mousePosNormalize;
+
+        var targetRotationY = Quaternion.AngleAxis(deltaMousePos.x * rotateVelocity, transform.up);
+        rBody.rotation *= targetRotationY;
     }
 
 }
